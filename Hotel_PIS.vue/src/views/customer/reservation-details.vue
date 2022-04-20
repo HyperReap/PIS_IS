efadf9d3951480b496a596c13770b5689c29bd25
xstudn00 <studnicka39@seznam.cz>
2022-04-20 21:49:49 +0200

    create reservation: TODO list

--- Hotel_PIS.vue/src/views/customer/reservation-details.vue
+++ Hotel_PIS.vue/src/views/customer/reservation-details.vue
@@ -1,5 +1,14 @@
 <template>
     <h2>TODO: Detaily ohledně rezervace</h2>
+    <ul>
+        <li>zobrazit zde detaily ohledne vybranych pokoju, tak jako na strance se vsemi pokoji</li>
+        <li>donutit uživatele vybrat správně datum</li>
+        <li>při vybírání data už musí být vypnuté obsazené termíny</li>
+        <li>donutit ho vyplnit všechy potřebné údaje (jmeno, prijmeni, email, telefon, pocet osob - nesmi byt vyssi nez max pocet osob na pokoji/pokojich)</li>
+        <li>odesilani na BE 2x (prvne klient, melo by se vratit jeho id ; pak rezervace, mel bych poslat id klienta)</li>
+        <li>mel bych si do store ulozit email klienta pro pouziti v mych rezervacich</li>
+        <li>mel bych presmerovat na stranku s detaily rezervace</li>
+    </ul>
 </template>
 <script lang="js">
     export default {
