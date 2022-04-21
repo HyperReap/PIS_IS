976e23b1cd526a2844d1f3bdc21e1e11598a37e8
xstudn00 <studnicka39@seznam.cz>
2022-04-21 18:27:25 +0200

    template

---
+++ Hotel_PIS.vue/src/views/template.vue
@@ -0,0 +1,71 @@
+<template>
+    <!-- Tady se pise HTML -->
+    <!--
+        <neco />
+        <p>nejaky odstavec</p>
+        pro binding nebo vue direktivy kouknete trerba na rooms.vue nebo filters.vue
+        dalsi info zde: https://v1.vuejs.org/guide/syntax.html
+        pripadne piste
+    -->
+</template>
+<script lang="js">
+    export default {
+        /*
+         @ znaci root
+        import neco from '@/views/...'
+        components: {
+            neco,
+        },
+        nejaka data od parenta
+        props: ['filterValues'],
+        data() {
+            return {
+                neco: null,
+                nejakyObjekt:{
+                    neco1: null,
+                    neco2:null
+                }
+            };
+        },
+        created() {
+            this.metoda();
+        },
+        mounted() {
+            this.metoda();
+        },
+        methods: {
+            metoda(){
+                ...
+            }
+        },
+        computed: {
+            test(){
+                return this.neco + "Ahoj"
+            }
+        },
+        watch: {
+            neco(newValue, oldValue) {
+                ...
+            },
+            someObject: {
+              handler(newValue, oldValue) {
+                ...
+              },
+              deep: true
+            }
+        },
+        v pripade potreby uchovavat nejaka data, tak je potreba pouzit store
+        v pripade potreby vytvoreni noveho url, tak je potreba pouzit router
+
+        v obou pripadech tam uz jsou nejake ukazky, ale radsi napiste
+
+        
+        */
+    }
+</script>
+<style scoped>
+    /*
+        Tady se pisou styly pouze pro toto view (pripadne komponentu)
+        Pokud je potreba overridnout styly nize postavene komponenty, tak pouzit >>>
+    */
+</style>
