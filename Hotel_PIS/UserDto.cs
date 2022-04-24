0b0611dcabfa35911c8c2e7b681ac3b73da325a1
Jakub Kubik <jakupkubik@gmail.com>
2022-04-24 17:12:32 +0200

    Add JWT token

---
+++ Hotel_PIS/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Hotel_PIS
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
